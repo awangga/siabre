f9c3b24b0a458874c6af1ebb3f747605fedccfb1
Rolly Maulana  Awangga <rolly@awang.ga>
2022-10-19 12:40:19 +0700

    fix argument

--- omniglot-result.py
+++ omniglot-result.py
@@ -1,11 +1,16 @@
 from lib.omniglot import make_oneshot_task,plot_oneshot_task
 import pickle
-
+import os
 
 save_path = './data/'
 
 with open(os.path.join(save_path, "accuracies.pickle"), "rb") as f:
     (val_accs, train_accs, nn_accs) = pickle.load(f)
 
-pairs, targets = make_oneshot_task(16,"train","Sanskrit")
+with open(os.path.join(save_path, "val.pickle"), "rb") as f:
+    (Xval, val_classes) = pickle.load(f)
+with open(os.path.join(save_path, "train.pickle"), "rb") as f:
+    (Xtrain, train_classes) = pickle.load(f)
+    
+pairs, targets = make_oneshot_task(16,Xtrain,Xval,train_classes,val_classes,"train","Sanskrit")
 plot_oneshot_task(pairs)
